f6e572634cf12583d47cc10539c64cc8f11062db
Officefront <officefront@imac-de-officefront.local>
2016-03-23 10:59:35 -0500

    Part 7 completed

--- MusicVideo/Constants.swift
+++ MusicVideo/Constants.swift
@@ -12,3 +12,11 @@ typealias JSONDictionary = [String: AnyObject]
 
 typealias JSONArray = Array<AnyObject>
 
+let WIFI = "WIFI Available"
+
+let NOACCESS = "No Internet Access"
+
+let WWAN = "Cellular Access Available"
+
+
+
